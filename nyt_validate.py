ee3885f8db70bd6c7ef11569895f98a3b648f84e
Josh Quan <wrathofquan@gmail.com>
2021-07-30 13:42:45 -0700

    Update nyt_validate.py

--- nyt_validate.py
+++ nyt_validate.py
@@ -6,7 +6,6 @@ from lxml import etree, objectify
  # and the files 'nitf-3-3.dtd' 'xhtml-ruby-1.mod' are in working directory
 
 def validateXML(path):
-    parser = etree.XMLParser(dtd_validation=True, no_network=True)
     dtd = etree.DTD(open('nitf-3-3.dtd', 'rb'))
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -14,6 +13,7 @@ def validateXML(path):
                 fname = os.path.join(root, file)
                 try:
                     tree = objectify.parse(open(fname, 'rb'))
+                    print(dtd.validate(tree))
                 except etree.XMLSyntaxError as err:
                     print(err)
 
