cf80c5a52f9c7609cc54ae9e9037d22d38b29b8e
Josh Quan <wrathofquan@gmail.com>
2021-07-29 17:35:17 -0700

    initial commit

---
+++ nyt_validate.py
@@ -0,0 +1,23 @@
+import os
+from lxml import etree, objectify
+
+
+ # for this to work make sure: tarfiles are uncompressed, 
+ # and the files 'nitf-3-3.dtd' 'xhtml-ruby-1.mod' are in working directory
+
+def validateXML(path):
+    parser = etree.XMLParser(dtd_validation=True, no_network=True)
+    dtd = etree.DTD(open('nitf-3-3.dtd', 'rb'))
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.xml'):
+                fname = os.path.join(root, file)
+                try:
+                    tree = objectify.parse(open(fname, 'rb'))
+                except etree.XMLSyntaxError as err:
+                    print(err)
+
+validateXML("/path/to/xml/files")
+
+
+
